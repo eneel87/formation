d8d2c8ffe2577d480a02ba904d4c96a34ef3cff9
eneel87 <e.neel@dreamcentury.com>
2016-01-26 10:33:05 +0100

    ajout d'une vue menu qui est inclue par le layout

--- App/Backend/Templates/layout.php
+++ App/Backend/Templates/layout.php
@@ -18,19 +18,7 @@
         <p>Comment ça, il n'y a presque rien ?</p>
       </header>
       
-      <nav>
-        <ul>
-          <li><a href="/">Accueil</a></li>
-          <?php if (!$user->isAuthenticated() ) {?>
-          <li><a href="/admin/">Connexion</a></li> <?php } ?>
-          <?php if ($user->isAuthenticated()) { ?>
-          <?php if($user->getAttribute('admin')->level()==1){ ?>
-          <li><a href="/admin/members.html">Gestion des utilisateurs</a></li><?php } ?>
-          <li><a href="/admin/news.html">Gestion des news</a></li>
-          <li><a href="/admin/deconnexion.html">Déconnexion de <?php echo htmlspecialchars($user->getAttribute('admin')->login()); } ?></a></li>
-
-        </ul>
-      </nav>
+      <?= $menu ?>
       
       <div id="content-wrap">
         <section id="main">
--- lib/OCFram/Page.php
+++ lib/OCFram/Page.php
@@ -6,6 +6,7 @@ class Page extends ApplicationComponent
   protected $template;
   protected $contentFile;
   protected $vars = [];
+  protected $menu;
 
   public function __construct(Application $App)
   {
@@ -38,9 +39,30 @@ class Page extends ApplicationComponent
       require $this->contentFile;
     $content = ob_get_clean();
 
+    $menu = $this->getMenu();
+
     ob_start();
       require __DIR__.'/../../App/'.$this->app->name().'/Templates/'.$this->template;
     return ob_get_clean();
+  }
+
+  public function getMenu()
+  {
+    $menu_view = __DIR__.'/../../App/'.$this->app->name().'/Templates/menu.php';
+
+    if (!file_exists($menu_view))
+    {
+      throw new \RuntimeException('La vue spécifiée pour le menu n\'existe pas');
+    }
+
+    $router = new Router();
+    $user = $this->app->user();
+
+    ob_start();
+      require $menu_view;
+    $menu = ob_get_clean();
+
+    return $menu;
   }
 
   public function setContentFile($contentFile)
